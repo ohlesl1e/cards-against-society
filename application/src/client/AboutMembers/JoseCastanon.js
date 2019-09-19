f01cea52e6eed7ba168b5b96a85d7c9c2c85fa53
Jose C <jcastan6@mail.sfsu.edu>
2019-09-19 01:55:04 -0700

    added m0, fixed file layout

--- application/src/client/AboutMembers/JoseCastanon.js
+++ application/src/client/AboutMembers/JoseCastanon.js
@@ -1,26 +1,31 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 const backButton = {
-    background: "white",
-    color: "#1b1b1e",
-    border: "none",
-    cursor: "pointer",
-    fontSize: "12px",
-    textDecoration: 'none'
-}
+  background: 'white',
+  color: '#1b1b1e',
+  border: 'none',
+  cursor: 'pointer',
+  fontSize: '12px',
+  textDecoration: 'none'
+};
 
 class JoseCastanon extends React.Component {
-    render() {
-        return ( 
-            <body>
-              <Link style={backButton} to="About">to About</Link>
-              <div style={{marginLeft: "10vh"}}>
-                <h1>
-                    Jose Castanon
-                </h1>
-              </div>
-            </body>
-        );
-    }
-} export default JoseCastanon;
+  render() {
+    return (
+      <body>
+        <Link style={backButton} to="About">
+          to About
+        </Link>
+        <div style={{ marginLeft: '10vh' }}>
+          <h1>Jose Castanon</h1>
+          <p>
+            Jose is a senior Computer Science undergraduate at San Francisco
+            State University.
+          </p>
+        </div>
+      </body>
+    );
+  }
+}
+export default JoseCastanon;
--- application/src/server/index.js
+++ application/src/server/index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const os = require("os");
-const path = require("path");
+const express = require('express');
+const os = require('os');
+const path = require('path');
 
 const app = express();
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
-app.listen(8080, () => console.log("Listening on port 4000!"));
+app.listen(8080, () => console.log('Listening on port 4000!'));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"), err => {
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'), (err) => {
     if (err) {
-      console.log("error");
+      console.log('error');
       res.status(500).send(err);
     }
   });
