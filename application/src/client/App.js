8ce9c6ebbff87688d7617f2c378e04350f1c48ac
Jose C <jcastan6@mail.sfsu.edu>
2019-09-18 23:32:35 -0700

    fixed page refresh issue

--- application/src/client/App.js
+++ application/src/client/App.js
@@ -1,32 +1,30 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 const backButton = {
-  background: "white",
-  color: "#1b1b1e",
-  border: "none",
-  cursor: "pointer",
-  fontSize: "12px",
+  background: 'white',
+  color: '#1b1b1e',
+  border: 'none',
+  cursor: 'pointer',
+  fontSize: '12px',
   textDecoration: 'none'
-}
+};
 
-class App extends React.Component {
+export default class App extends Component {
   render() {
-    return(
+    return (
       <body>
         <h1>
-          <center>
-            Cards Against Society
-          </center>
+          <center>Cards Against Society</center>
         </h1>
         <h4>
-          <center>
-          CSC648-04 / Fall 2019 / Team 203
-          </center>
+          <center>CSC648-04 / Fall 2019 / Team 203</center>
         </h4>
-        <Link style={backButton} to="About">to About</Link>
-        <p>
-        </p>
+        <Link style={backButton} to="About">
+          to About
+        </Link>
+        <p />
       </body>
-    )}
-} export default App;
+    );
+  }
+}
