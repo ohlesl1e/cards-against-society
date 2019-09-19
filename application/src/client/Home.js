a36183a998c61cf38f2479eb07c8a77850445b4e
Jose C <jcastan6@mail.sfsu.edu>
2019-09-18 23:35:48 -0700

    Fixed file layout

---
+++ application/src/client/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const backButton = {
+  background: 'white',
+  color: '#1b1b1e',
+  border: 'none',
+  cursor: 'pointer',
+  fontSize: '12px',
+  textDecoration: 'none'
+};
+
+export default class Home extends Component {
+  render() {
+    return (
+      <body>
+        <h1>
+          <center>Cards Against Society</center>
+        </h1>
+        <h4>
+          <center>CSC648-04 / Fall 2019 / Team 203</center>
+        </h4>
+        <Link style={backButton} to="About">
+          to About
+        </Link>
+        <p />
+      </body>
+    );
+  }
+}
