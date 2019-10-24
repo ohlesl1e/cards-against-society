465f57126ca7ab3b2f524ad7374bf1e12b25f8b8
Jose Castanon - Laptop <jcastan6@mail.sfsu.edu>
2019-10-24 00:36:54 -0700

    added models for cards and games

---
+++ application/src/server/models/cards.js
@@ -0,0 +1,40 @@
+/*
+*  Author: Jonathan Julian
+*  Author: Inez Wibowo
+*  Purpose: DB model for all game session activity
+*/
+
+"use strict";
+
+module.exports = (sequelize, Sequelize) => {
+  const card = sequelize.define(
+    "card",
+    {
+      id: {
+        type: Sequelize.INTEGER(9),
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+      },
+      text: {
+        type: Sequelize.TEXT("long"),
+        allowNull: false,
+      },
+      draw: {
+        type: Sequelize.INTEGER(6),
+        allowNull: false,
+        default: 0
+      },
+      pick: {
+        type: Sequelize.INTEGER(6),
+        allowNull: false,
+        default: 1,
+      }
+      
+    },
+    {
+      updatedAt: false
+    }
+  );
+  return card;
+};
---
+++ application/src/server/models/games.js
@@ -0,0 +1,43 @@
+/*
+*  Author: Jonathan Julian
+*  Author: Inez Wibowo
+*  Purpose: DB model for all game session activity
+*/
+
+"use strict";
+
+module.exports = (sequelize, Sequelize) => {
+  const gamesession = sequelize.define(
+    "gamesessions",
+    {
+      gameid: {
+        type: Sequelize.INTEGER(11),
+        autoIncrement: true,
+        primaryKey: true
+      },
+
+      gameState: {
+        type: Sequelize.TEXT("long"),
+        allowNull: false
+      },
+      
+    },
+    {
+      updatedAt: false
+    }
+  );
+
+  gamesession.associate = models => {
+    gamesession.belongsTo(models.user, {
+      as: "Host",
+      through: "hostTable"
+    });
+
+    gamesession.belongsTo(models.user, {
+      as: "Player",
+      through: "playerTable"
+    });
+  };
+
+  return gamesession;
+};
