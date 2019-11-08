dd2db2b10cc92c93e09ddb354b87db7df9611175
Jose Castanon - Laptop <jcastan6@mail.sfsu.edu>
2019-11-07 20:26:00 -0800

    added mygames page

---
+++ application/src/client/MyGames.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Header from './Components/Header';
+import { retrieveCookie } from "./Components/cookies";
+
+export default class MyGames extends Component {
+    state = {
+        userid: retrieveCookie("userid")
+    }
+
+    componentDidMount(){
+        if(!retrieveCookie("userid")){
+            alert("please log in!");
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                
+            </div>
+        );
+  }
+}
