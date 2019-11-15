e732c7dc5f3b886b7399185283fa2028347381cf
darylortiz100 <dortiz4@mail.sfsu.edu>
2019-11-15 14:27:33 -0800

    Fix merge issues, updated lobby, profile, home and app.css

--- application/src/client/Home.js
+++ application/src/client/Home.js
@@ -1,7 +1,6 @@
 
 import React, { Component } from 'react';
 import Header from './Components/Header';
-import { Link } from "react-router-dom";
 import "./app.css";
 
 export default class Home extends Component {
