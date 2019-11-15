86989ca4a8a65e3852ade1bdd84c7400f63efd4b
Leslie Zhou <zzhou2@mail.sfsu.edu>
2019-11-15 13:11:04 -0800

    Custom card page change

--- application/src/client/App.js
+++ application/src/client/App.js
@@ -14,7 +14,8 @@ import Game from './Game';
 import Lobby from './Lobby';
 import Login from './Login';
 import MyGames from './MyGames';
-import Custom from './Custom';
+import AddCustom from './AddCustom';
+import Custom from './Custom'
 
 // For now root path
 // If error for react-router-dom, install "npm install react-router-dom"
@@ -56,6 +57,7 @@ export default class Routing extends Component {
         <Route component={Game} path="/Game" />
         <Route component={Lobby} path="/Lobby" />
         <Route component={MyGames} path="/MyGames" />
+        <Route component={AddCustom} path="/AddCustom" />
         <Route component={Custom} path="/Custom" />
       </BrowserRouter>
     );
