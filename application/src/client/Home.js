96228d210a7d1f7b9053306d8cc3891de719ff94
darylortiz100 <dortiz4@mail.sfsu.edu>
2019-11-15 13:34:58 -0800

    Updated Home, Lobby and app.css for styling

--- application/src/client/Home.js
+++ application/src/client/Home.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import Header from './Components/Header';
 import { Link } from "react-router-dom";
@@ -17,21 +18,7 @@ export default class Home extends Component {
         </h4>
       
       
-        <div class="row">
-          <div class= "column">
-          <p>Active Game Rooms:</p>
-          <h3>Game Rooms Active</h3>
-          <p></p>
-          </div>
-          <div class ="column">
-            <h3>About The Game</h3>
-            <p>
-              Cards Against Society is a party game based on Cards Against Humanity in which players complete 
-              fill-in-the-blank statements using words or phrases typically deemed as offensive, risqué or politically, incorrect printed on playing cards. 
-              It has been compared to the 1999 card game Apples to Apples and originated from a Kickstarter campaign in 2011.
-              </p>
-          </div>
-          </div>
+       
       </body>
     );
   }
