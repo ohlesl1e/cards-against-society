cfa6a8758486120ba2678fd71afec264d5b0d3e3
Jose C <jcastan6@mail.sfsu.edu>
2019-11-14 18:21:18 -0800

    fixed chatbox component

--- application/src/client/Game.js
+++ application/src/client/Game.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import Header from './Components/Header';
+import { retrieveCookie } from './Components/cookies';
+import Chatbox from './Components/Chatbox';
 
 export default class Game extends Component {
-    state = {
+  state = {
+    userid: retrieveCookie('userid')
+  };
 
-    }
-
-    render() {
-        return (
-            <div>
-                <Header />
-                
-            </div>
-        );
+  render() {
+    return (
+      <div>
+        <Header userid={this.state.userid} />
+        <Container>
+          <br />
+          <Row>
+            <Col md="3">
+              <div className="text-center">
+                <Chatbox userid={this.state.userid} />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
