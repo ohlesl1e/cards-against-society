a6e7092ced067f29590db4e69d86b0c5912264d9
darylortiz100 <dortiz4@mail.sfsu.edu>
2019-11-17 14:24:02 -0800

    Resolving merge conflict in Lobby.js

--- application/src/client/Lobby.js
+++ application/src/client/Lobby.js
@@ -18,7 +18,6 @@ export default class Lobby extends Component {
 
   render() {
     return (
-<<<<<<< HEAD
       <body>
         <div className="homePage">
          <Header userid={this.state.userid} />
@@ -32,40 +31,6 @@ export default class Lobby extends Component {
               <Chatbox userid={this.state.userid} />
           </div>
       </body>
-=======
-      <div className="homePage">
-        <Header userid={this.state.userid} />
-
-        <body>
-          <h1>
-            <center>Cards Against Society</center>
-          </h1>
-
-          <div className="row">
-            <div className="column2">
-              <p>Active Game Rooms</p>
-              <ListRooms url="http://localhost:4000/game/allgames" />
-              <p />
-            </div>
-            <div className="column">
-              <h3>About The Game</h3>
-              <p>
-                Cards Against Society is a party game based on Cards Against
-                Humanity in which players complete fill-in-the-blank statements
-                using words or phrases typically deemed as offensive, risqué or
-                politically, incorrect printed on playing cards. It has been
-                compared to the 1999 card game Apples to Apples and originated
-                from a Kickstarter campaign in 2011.
-              </p>
-
-              <div className="text-center chatbox">
-                <Chatbox userid={this.state.userid} url="lobby" />
-              </div>
-            </div>
-          </div>
-        </body>
-      </div>
->>>>>>> 3932cb8d074424659f0d59951e67d453ad96354c
     );
   }
 }
