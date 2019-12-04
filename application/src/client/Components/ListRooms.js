7e0e8289f7a347d7de9fb3f09f31e0f48b8d111b
Jose C <jcastan6@mail.sfsu.edu>
2019-12-03 19:24:53 -0800

    added room search, pagination

--- application/src/client/Components/ListRooms.js
+++ application/src/client/Components/ListRooms.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "../app.css";
 import { Table, Button } from "react-bootstrap";
 import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
 import io from "socket.io-client";
 import { retrieveCookie } from "./Cookies";
 
@@ -63,6 +65,7 @@ export default class ListRooms extends Component {
 
   // Create a table function
   createTable = () => {
+    const { SearchBar } = Search;
     if (this.state.data !== null) {
       if (this.state.data.length === 0) {
         children.push(<div>No games available... please create one.</div>);
@@ -74,25 +77,49 @@ export default class ListRooms extends Component {
           },
           {
             dataField: "HostUserid",
-            text: "Host"
+            text: "Host",
+            searchable: false
           },
           {
             dataField: "capacity",
             text: "Room Capacity",
-            formatter: this.roomCapacity.bind(this)
+            formatter: this.roomCapacity.bind(this),
+            searchable: false
           },
           {
             dataField: "button",
             text: "Join",
-            formatter: this.cellButton.bind(this)
+            formatter: this.cellButton.bind(this),
+            searchable: false
           }
         ];
         return (
-          <BootstrapTable
+          <ToolkitProvider
             keyField="roomName"
             data={this.state.data}
             columns={columns}
-          />
+            pagination={paginationFactory()}
+            search
+          >
+            {props => (
+              <div>
+                <center>
+                  <h3>Game Rooms Active</h3>
+                </center>
+
+                <hr />
+                <SearchBar
+                  {...props.searchProps}
+                  placeholder="Search for a room"
+                />
+
+                <BootstrapTable
+                  {...props.baseProps}
+                  pagination={paginationFactory()}
+                />
+              </div>
+            )}
+          </ToolkitProvider>
         );
       }
     }
